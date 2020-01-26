91ce50a8473e0a7653e463ce10e9e8bb9e51653c
Miles Higbie <miles.higbie@gmail.com>
2020-01-26 16:05:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 class Application
 # item_name = req.path.split("/items/").last # - is a string 
 # @@items array - returns instances 
 
 @@items = []
+=======
+require "pry"
+
+class Application
+>>>>>>> edcffb5c2446390743867f7b1a620b3950fad694
 
     def call(env)
         resp = Rack::Response.new
@@ -10,6 +16,7 @@ class Application
 
         if req.path.match(/items/)
             item_name = req.path.split("/items/").last
+<<<<<<< HEAD
             if @@items.find {|item| item.name == item_name}
                 matching_item_instance = @@items.find {|item| item.name == item_name}
                 resp.write matching_item_instance.price 
@@ -20,6 +27,11 @@ class Application
         else 
             resp.status = 404
             resp.write "Route not found"
+=======
+            if @@items.include?(item_name)
+                return item_name.price
+            end 
+>>>>>>> edcffb5c2446390743867f7b1a620b3950fad694
         end 
         resp.finish
     end 
